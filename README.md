180ad95b4a5863fd3c90e15880f09f974361d0ed
Brad Sneade <bsneade@gmail.com>
2016-05-11 19:55:02 -0400

    initial refactoring

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # pgwebmin
 Web based administrative interface for PostgreSQL
-
-npm install -g gulp
-
-## Running the dev server
-gulp develop
--- pgwebmin-backend/routes/connectionRoutes.js
+++ pgwebmin-backend/routes/connectionRoutes.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var pg = require('pg');
+var logger = require('winston').loggers.get('ConnectionModel');
 var ConnectionControler = require('../controllers/connectionController');
 
 function ConnectionRoutes(server) {
@@ -7,17 +8,23 @@ function ConnectionRoutes(server) {
   server.get('/connection', ConnectionControler.getConnections);
   
   server.post('/sql', function (req, res, next) {
-	var client = new pg.Client("postgres://brad:password1$@localhost");
+	var client = new pg.Client({
+      user: 'brad',
+      password: 'password1$',
+      database: 'brad',
+      host: 'localhost',
+      port: 5432
+    });
 	client.connect(function(err) {
 	  	if(err) {
-	    	return console.error('could not connect to postgres', err);
+	    	return logger.error('could not connect to postgres', err);
 	  	}
-	  	console.log("Executing " + req.body.sql);
+	  	logger.debug("Executing " + req.body.sql);
 	  	client.query(req.body.sql, function(err, result) {
 		    if(err) {
-		    	return console.error('error running query', err);
+		    	return logger.error('error running query', err);
 		    }
-		    console.log("Returning rows " + result.rows[0]);
+		    logger.info("Returning rows " + result.rows[0]);
 		    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
 		    client.end();
 		});
