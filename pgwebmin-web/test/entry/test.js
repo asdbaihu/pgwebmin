7accc9bf8b57fc2cef2d4878d68be8b9055c0896
Brad Sneade <bsneade@gmail.com>
2016-10-04 22:49:51 -0400

    clearing out old hacked up code

--- pgwebmin-web/test/entry/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Polyfills for phantomjs */
-require('console-polyfill');
-require('phantomjs-polyfill');
-require('babel-polyfill');
-
-/* Test spec files */
-//require('my-first-store-spec');
